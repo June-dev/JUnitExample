36988790834e7195cc3ca0715f87afe667beeb9a
yunjy-tmax <junyeon_yun@tmax.co.kr>
2020-09-09 20:45:28 +0900

    [NEW] Test case with setUp()

---
+++ JUnitExample/src/com/wordboundary/test/WordBoundaryIteratorTest.java
@@ -0,0 +1,29 @@
+package com.wordboundary.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WordBoundaryIteratorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCurrent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFollowing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNext() {
+		fail("Not yet implemented");
+	}
+
+}
